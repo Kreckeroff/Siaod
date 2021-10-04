afe99a0394edaba28e213c0ef4167b7a5c5fd111
Egor <egorgrebenn@gmail.com>
2021-10-04 21:35:47 +0300

    add class
    omega ZULUL code

--- day2/main.cpp
+++ day2/main.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 
+
 int ft_atoi(char* str)
 {
     int flag;
@@ -59,43 +60,50 @@ Node* node = new Node;
 node->next = creator(count - 1);
 return node;
 }*/
+class HashTable
+{
+private:
+   const int N = 3;
+public:
+    int i = 0;
+    HashTable()
+    {
+        Product* product[N];
+        while (i < N)
+        {
+             product[i] = NULL;
+             i++;
+        }
+        printf("Введите %d записей товаров", N);
+        for (int i = 0; i < N; i++)
+        {
+            Product* newnode = new Product;
+            scanf("%s", &newnode->name);
+            scanf("%s", &newnode->key);
+            int hashnum = HashFunc(newnode->key);
+            Product* buffnode = product[hashnum];
+            if (buffnode != NULL)
+            {
+                int k = 2;
+                while (buffnode->next != NULL)
+                {
+                    buffnode = buffnode->next;
+                    k++;
+                }
+                buffnode->next = newnode;
+                printf("Товар: %s, код: %s, записан по адресу %d, по счету %d\n", &newnode->name, &newnode->key, hashnum, k);
+            }
+            else
+            {
+                product[hashnum] = newnode;
+                printf("Товар: %s, код: %s, записан по адресу %d первым по счету\n", &product[hashnum]->name, &product[hashnum]->key, hashnum);
+                printf("%s", &product[hashnum]->name);
+            }
+        }
+    }
+};
 
 int main(int argc, char** argv)
 {
-    int i = 0;
-    const int N = 3;
-    Product* HashTable[N];
-    while (i < N)
-    {
-        HashTable[i] = NULL;
-        i++;
-    }
-    printf("Введите %d записей товаров", N);
-    for (int i = 0; i < N; i++)
-    {
-        Product* newnode = new Product;
-        scanf("%s", &newnode->name);
-        scanf("%s", &newnode->key);
-        int hashnum = HashFunc(newnode->key);
-        Product* buffnode = HashTable[hashnum];
-        if (buffnode != NULL)
-        {
-            int k = 2;
-            while (buffnode->next != NULL)
-            {
-                buffnode = buffnode->next;
-                k++;
-            }
-            buffnode->next = newnode;
-            printf("Товар: %s, код: %s, записан по адресу %d, по счету %d\n", &newnode->name, &newnode->key, hashnum, k);
-        }
-        else
-        {
-            HashTable[hashnum] = newnode;
-            printf("Товар: %s, код: %s, записан по адресу %d первым по счету\n", &HashTable[hashnum]->name, &HashTable[hashnum]->key, hashnum);
-            printf("%s", &HashTable[hashnum]->name);
-            //printf("%s",&HashTable[hashnum]->product.key);
-        }
-    }
-
+   HashTable();
 }
